393eaf222fbedad4ca6faeefdd678e43d46927c4
Dawn <danmanalo.gg@gmail.com>
2026-02-17 21:52:26 +0800

    Add function to accomplish step 2

---
+++ script.js
@@ -0,0 +1,27 @@
+// Project: Rock Paper Scissors
+
+// Step 1: Set up the project structure - DONE
+
+//Step 2: Write the logic to get the computer choice
+// Algorithm:
+// 1. Generate a random decimal value between 0 and 1.
+// 2. Split the value range into three equal intervals.
+// 3. Use conditional checks to determine which interval the value falls into.
+// 4. Output the corresponding choice.
+
+function getComputerChoice () {
+  const computerChoice = Math.random();
+  console.log(computerChoice);
+  if (computerChoice < 1/3) {
+    console.log("Rock");
+  }
+  else if (computerChoice < 2/3) {
+    console.log("Paper");
+  }
+  else {
+    console.log("Scissors")
+  }
+}
+
+getComputerChoice();
+
