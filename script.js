ef6a1243b067b46a6e1211619c5d0ca3ee206209
Dawn <danmanalo.gg@gmail.com>
2026-02-18 07:19:48 +0800

    Create a function to get human choice

--- script.js
+++ script.js
@@ -3,25 +3,34 @@
 // Step 1: Set up the project structure - DONE
 
 //Step 2: Write the logic to get the computer choice
-// Algorithm:
 // 1. Generate a random decimal value between 0 and 1.
 // 2. Split the value range into three equal intervals.
 // 3. Use conditional checks to determine which interval the value falls into.
 // 4. Output the corresponding choice.
 
 function getComputerChoice () {
+
   const computerChoice = Math.random();
-  console.log(computerChoice);
+  // console.log(computerChoice); - debugging for accurate conversion 
+
   if (computerChoice < 1/3) {
     console.log("Rock");
-  }
-  else if (computerChoice < 2/3) {
+  } else if (computerChoice < 2/3) {
     console.log("Paper");
-  }
-  else {
+  } else {
     console.log("Scissors")
   }
 }
 
 getComputerChoice();
+
+//Get human choice
+function getHumanChoice () {
+  const humanChoice = prompt("Select from Rock, Paper or Scissors");
+  return humanChoice;
+}
+
+// console.log(getHumanChoice()); for debugging 
+
+
 
